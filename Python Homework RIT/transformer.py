dd0aec13d9459702d41f9b2d1c3c2f27d1d2ef65
Bikash Roy <bikash0109@gmail.com>
2018-10-02 13:30:02 -0400

    New Changes

--- Python Homework RIT/transformer.py
+++ Python Homework RIT/transformer.py
@@ -1,13 +1,17 @@
 """REMOVE ALL PRINT STATEMENTS"""
 
 
-def shift_at_index(message, instruction):
+def shift(message, instruction, decrypt):
     index_to_shifted = int(instruction[1])
     by_times_forward = 1
+    if decrypt:
+        by_times_forward = -1
     condition_for_k_forward = "," in instruction and instruction.split(",")[1] != ""
     if condition_for_k_forward:
         split_message = instruction.split(",")
         by_times_forward = int(split_message[1])
+        if decrypt:
+            by_times_forward = -by_times_forward
     char_acsii_value = ord(message[index_to_shifted])
     if char_acsii_value in range(65, 91):
         if char_acsii_value == 90:
@@ -23,7 +27,7 @@ def shift_at_index(message, instruction):
     return new_message
 
 
-def rotate(message, instruction):
+def rotate(message, instruction, decrypt):
     if len(instruction) == 1:
         number_of_characters_to_rotates = 1
     else:
@@ -32,13 +36,16 @@ def rotate(message, instruction):
             number_of_characters_to_rotates = int(instruction[2])
             if number_of_characters_to_rotates > len(message):
                 return "Not enough string to rotate."
-            return message[len(message) - number_of_characters_to_rotates:] + message[0:len(
-                message) - number_of_characters_to_rotates]
         else:
             number_of_characters_to_rotates = int(instruction[1])
-            if number_of_characters_to_rotates > len(message):
-                return "Not enough string to rotate."
-    return message[len(message) - number_of_characters_to_rotates:] + message[:-number_of_characters_to_rotates]
+    if decrypt:
+        for i in range(0, number_of_characters_to_rotates):
+            message = message[1:] + message[0]
+        return message
+    else:
+        for i in range(0, number_of_characters_to_rotates):
+            message = message[-1] + message[:-1]
+        return message
 
 
 def duplicate(message, instruction):
@@ -55,13 +62,15 @@ def duplicate(message, instruction):
            message[index_to_be_duplicated + 1:]
 
 
-def swap(message, instruction):
+def swap(message, instruction, decrypt):
     index_i = int(instruction[1])
     if index_i < 0:
         return "No element to swap. Index smaller than message length"
     index_j = int(instruction.split(",")[1])
     if index_j > len(message):
         return "No element to swap. Index larger than message length"
+    if decrypt:
+        index_i, index_j = index_j, index_i
     i_character_to_be_swapped = message[index_i]
     j_character_to_be_swapped = message[index_j]
     message = list(message)
@@ -70,15 +79,17 @@ def swap(message, instruction):
     return "".join(message)
 
 
-def swap_with_limiter(message, instruction):
+def swap_groups(message, instruction, decrypt):
     index_i = int(instruction[4])
     if index_i < 0:
         return "No element to swap. Index smaller than message length"
     index_j = int(instruction.split(",")[1])
     if index_j > len(message):
         return "No element to swap. Index larger than message length"
+    if decrypt:
+        index_i, index_j = index_j, index_i
     number_of_groups_needed = int(instruction[2])
-    group_size = int(len(message)/number_of_groups_needed)
+    group_size = int(len(message) / number_of_groups_needed)
     message = [message[i:i + group_size] for i in range(0, len(message), group_size)]
     if index_j > len(message):
         return "Index larger than swapping groups"
@@ -89,12 +100,48 @@ def swap_with_limiter(message, instruction):
     return "".join(message)
 
 
+def encrypt_decrypt(message_filename, instruction_filename, decrypt):
+    message_file = open(message_filename, "r")
+    message_list = message_file.readlines()
+    instruction_filename = open(instruction_filename, "r")
+    instruction_list = instruction_filename.readlines()
+    output_list = ""
+    for i in range(0, len(message_list)):
+        message, instruction = message_list[i], instruction_list[i]
+        instruction = instruction.split(" ")
+        if decrypt:
+            instruction = instruction[::-1]
+        for j in range(0, len(instruction)):
+            if instruction[j].startswith("S"):
+                shift_message = shift(message.replace("\n", ""), instruction[j], decrypt)
+                message = shift_message
+            if instruction[j].startswith("R"):
+                rotate_message = rotate(message.replace("\n", ""), instruction[j], decrypt)
+                message = rotate_message
+            if instruction[j].startswith("D"):
+                duplicated_message = duplicate(message.replace("\n", ""), instruction[j])
+                message = duplicated_message
+            if instruction[j].startswith("T"):
+                swapped_message = swap(message.replace("\n", ""), instruction[j], decrypt)
+                message = swapped_message
+            if instruction[j].startswith("T("):
+                swapped_group_message = swap_groups(message.replace("\n", ""), instruction[j], decrypt)
+                message = swapped_group_message
+        output_list += message.replace("\n", "") + "\n"
+    output_file = open("output.txt", "w")
+    output_file.writelines(output_list)
+    output_file.close()
+    message_file.close()
+    instruction_filename.close()
+
+
 def main():
-    shift_at_index("Z", "S0,-4")
-    rotate("SEHOR", "R-3")
-    duplicate("HOPED", "D2,4")
-    swap("SAUCE", "T3,1")
-    swap_with_limiter("BACKHANDES", "T(4)0,2")
+    # shift("Z", "S0,-4")
+    # rotate("SEHOR", "R-3")
+    # duplicate("HOPED", "D2,4")
+    # swap("SAUCE", "T3,1")
+    # swap_groups("BACKHANDES", "T(4)0,2")
+    encrypt_decrypt("message.txt", "instruction.txt", False)
 
 
 if __name__ == '__main__':
